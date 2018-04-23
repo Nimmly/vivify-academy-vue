c1cf0a2ac11489b227268b75e09fe6ee52c6671e
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-04-23 20:59:37 +0200

    first

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Email</th>
+      </thead>
+      <tbody>
+        <tr v-for= "(user, key) in users" :key="key">
+          <td>{{ user.first_name }}</td>
+          <td>{{ user.last_name}}</td>
+          <td>{{ user.email}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactsList',
+  data() {
+    return {
+      users: [
+        { first_name: 'Marko', last_name: 'Popovic',  email: 'nesto@exm.com'},
+        { first_name: 'Marko1', last_name: 'Popovic1', email: 'nesto1@exm.com'},
+        { first_name: 'Marko2',  last_name: 'Popovic2', email: 'nesto2@exm.com'}
+      ]
+    }
+  }
+}
+</script>
+
+
