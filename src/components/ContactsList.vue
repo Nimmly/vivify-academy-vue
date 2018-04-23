d074576c4bfa02ac40c963d65453dbdd3b657d2e
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-04-23 21:39:54 +0200

    third

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <form @submit.prevent="addContact">
+      <label>First name</label>
+      <input v-model="newContact.first_name" type='text' placeholder="Firstname"/>
+      <label>Last name</label>
+      <input v-model="newContact.last_name" type='text' placeholder="Lastname"/>
+      <label>Email</label>
+      <input v-model="newContact.email" type="email" placeholder="Email"/>
+      <button type='submit'>Submit</button>
+      </form>
     <table>
       <thead>
         <th>First name</th>
@@ -7,7 +16,7 @@
         <th>Email</th>
       </thead>
       <tbody>
-        <tr v-for= "(contact, key) in contacts" :key="key" v-if="contact.first_name ==='Marko'">
+        <tr v-for= "(contact, key) in contacts" :key="key">
           <td>{{ contact.first_name }}</td>
           <td>{{ contact.last_name}}</td>
           <td>{{ contact.email}}</td>
@@ -28,10 +37,22 @@ export default {
         { first_name: 'Marko', last_name: 'Popovic',  email: 'nesto@exm.com'},
         { first_name: 'Marko1', last_name: 'Popovic1', email: 'nesto1@exm.com'},
         { first_name: 'Marko2',  last_name: 'Popovic2', email: 'nesto2@exm.com'}
-      ]
+      ],
+      newContact: {
+        first_name:'',
+        last_name:'',
+        email:''
+      }
+    }
+  },
+  methods:{
+    addContact(){
+      this.contacts.push(this.newContact)
+      this.newContact = {}
+      }
     }
   }
-}
+
 </script>
 
 
